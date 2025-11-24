be6769cbf1bac77738885f75a74869fa29a45c33
IniOluwa Longe <inilonge@gmail.com>
2025-11-24 23:16:54 +0100

    Add CustomAppBar widget and integrate it in DiscoverScreen

---
+++ lib/view/widgets/appbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:jollycast/view/widgets/color.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+    this.title,
+    this.actions,
+    this.leading,
+  });
+
+  final Widget? title;
+  final List<Widget>? actions;
+  final Widget? leading;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: blackColor3,
+      elevation: 0,
+      leading: leading,
+      title: title,
+      actions: actions,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
