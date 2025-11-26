4b0bbf4c295cacd222299442c4f41270a61f9e05
IniOluwa Longe <inilonge@gmail.com>
2025-11-26 03:49:59 +0100

    style: format test file with dart format

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,9 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:jollycast/main.dart';
 
 void main() {
-  testWidgets('App starts and shows LoginScreen', (
-    WidgetTester tester,
-  ) async {
+  testWidgets('App starts and shows LoginScreen', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
     await tester.pumpAndSettle();
